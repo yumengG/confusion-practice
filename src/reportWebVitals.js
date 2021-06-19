e6b5ef016ae27a6d44ceaac4a1d50ec3d7aaa036
Yumeng <yumenglljj@gmail.com>
2021-06-18 21:27:00 -0700

    navbar

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+/* eslint-disable semi */
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
