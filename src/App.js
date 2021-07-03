2c1b006c518f1985897155a46844d1e7560f32f6
Yumeng <yumenglljj@gmail.com>
2021-07-02 18:02:52 -0700

    add new menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css'
-import Header from './Header'
 import Menu from './components/MenuComponent'
 import { DISHES } from './shared/Dishes'
+import Main from './components/MainComponent'
 
 function App () {
   return (
     <div className="App">
-      <Header />
-      <Menu dishes={DISHES}/>
+      <Main />
     </div>
   )
 }
